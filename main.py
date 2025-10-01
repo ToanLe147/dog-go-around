7792c259dda158893c744ef981059ea1594f1d2a
Toan Le <normalboyinlife@gmail.com>
2025-10-01 18:31:45 +0300

    something new for the dog

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+from ursina import *
+from src.game import Game
+
+# Global game instance
+game_instance = None
+
+def update():
+    if game_instance:
+        game_instance.update()
+
 def main():
-    print("Hello from dog-go-around!")
+    app = Ursina()
+    window.title = 'Simple 3D Game'
+    window.borderless = False
+    window.fullscreen = False
+    window.exit_button.visible = False
+    window.fps_counter.enabled = True
 
+    global game_instance
+    game_instance = Game()
 
-if __name__ == "__main__":
+    app.run()
+
+if __name__ == '__main__':
     main()
+
